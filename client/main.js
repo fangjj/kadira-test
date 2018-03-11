628620bbf9bf899427069ff18f290dc6836f8040
jm <jm@jmdeMac-mini.local>
2018-03-11 14:59:46 +0800

    add something

--- client/main.js
+++ client/main.js
@@ -1,23 +1,30 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import '../imports/api/tasks.js';
+import { Tasks } from '../imports/api/tasks.js';
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.body.onCreated(function bodyOnCreated() {
+  Meteor.subscribe('tasks');
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+ 
+    // Insert a task into the collection
+    Meteor.call('tasks.insert', text);
+ 
+    // Clear form
+    target.text.value = '';
   },
 });
--- server/main.js
+++ server/main.js
@@ -4,5 +4,6 @@ import '../imports/api/tasks.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  //Kadira.connect('tt5X7E8Zv7yrgzcDW', '7905a79f-077b-421a-bd6c-ca1cd71e24d6', {endpoint: 'http://119.29.113.71:11011'});
+  //Kadira.connect('tt5X7E8Zv7yrgzcDW', '7905a79f-077b-421a-bd6c-ca1cd71e24d2', {endpoint: 'http://meteor.21gcafe.com'});
+  // 要调试并监控生产坏境的meteor App性能，需要分摊维护自建kadira费用，有问题请联系我fangjimjim@qq.com，
 });
