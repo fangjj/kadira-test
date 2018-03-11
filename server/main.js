cf99c9a8ce72f69c4371c149edfdfeb97330e663
jm <jm@jmdeMac-mini.local>
2018-03-11 14:47:22 +0800

    kadira test

---
+++ server/main.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+
+import '../imports/api/tasks.js';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+  //Kadira.connect('tt5X7E8Zv7yrgzcDW', '7905a79f-077b-421a-bd6c-ca1cd71e24d6', {endpoint: 'http://119.29.113.71:11011'});
+});
