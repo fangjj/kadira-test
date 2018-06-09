44499f45fde229f9dbeee05f80635f60ac7e3279
jm <jm@jmdeMac-mini.local>
2018-06-09 22:11:53 +0800

    update readme

---
+++ readme.txt
@@ -0,0 +1,10 @@
+
+kadira 调试Demo指南
+
+#meteorhacks:kadira           #生产坏境下使用这个包，移除kadira:debug包，有问题请联系www.kadira@qq.com，
+kadira:debug                  #本地坏境下使用这个包，移除kadira:debug包，否则会导致安全问题
+
+// 本地坏境下调试，打开https://debug.kadira.cn/debug, 填入本地程序localhost:3000即可
+
+// 生产坏境的调试监控, 需要在www.kadira.cn, 新建app获取appid和appSecret
+//Kadira.connect('tt5X7E8Zv7yrgzcDW', '7905a79f-077b-421a-bd6c-ca1cd71e24d2', {endpoint: 'https://meteor.kadira.cn'});
--- server/main.js
+++ server/main.js
@@ -4,6 +4,9 @@ import '../imports/api/tasks.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  //Kadira.connect('tt5X7E8Zv7yrgzcDW', '7905a79f-077b-421a-bd6c-ca1cd71e24d2', {endpoint: 'http://meteor.21gcafe.com'});
-  // 要调试并监控生产坏境的meteor App性能，需要分摊维护自建kadira费用，有问题请联系我fangjimjim@qq.com，
+  // 本地坏境下调试，打开https://debug.kadira.cn/debug, 填入本地程序localhost:3000即可
+
+  // 生产坏境的调试监控, 需要在www.kadira.cn, 新建app获取appid和appSecret
+  //Kadira.connect('tt5X7E8Zv7yrgzcDW', '7905a79f-077b-421a-bd6c-ca1cd71e24d2', {endpoint: 'https://meteor.kadira.cn'});
+  
 });
